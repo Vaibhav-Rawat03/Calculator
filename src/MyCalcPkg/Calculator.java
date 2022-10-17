dde63eadf97c3deb4cc4a7deef8eba8867b2ac16
vaibhav <vaibhavrawat2003@gmail.com>
2022-10-17 13:11:40 +0530

    Multiplication fir

--- src/MyCalcPkg/Calculator.java
+++ src/MyCalcPkg/Calculator.java
@@ -6,4 +6,7 @@ int addition(int n1, int n2) {
 int Subtraction(int n1, int n2) {
 	return(n1-n2);
 }
+int Multiplication(int n1, int n2) {
+	return(n1*n2);
+}
 }
--- src/MyCalcPkg/MainCalc.java
+++ src/MyCalcPkg/MainCalc.java
@@ -7,8 +7,10 @@ public class MainCalc {
       Calculator c=new Calculator();
       int a=c.addition(10,20);
       int b=c.Subtraction(40,20);
+      int z=c.Multiplication(15, 30);
       System.out.println(a);
       System.out.println(b);
+      System.out.println(z);
 	}
 
 }
