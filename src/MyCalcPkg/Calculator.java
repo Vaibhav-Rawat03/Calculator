278e11b2d5337cda58fb8de979543aff7e2d7f4b
vaibhav <vaibhavrawat2003@gmail.com>
2022-10-17 13:09:27 +0530

    SUbtraction fir

--- src/MyCalcPkg/Calculator.java
+++ src/MyCalcPkg/Calculator.java
@@ -3,4 +3,7 @@ public class Calculator {
 int addition(int n1, int n2) {
 	return(n1+n2);
 }
+int Subtraction(int n1, int n2) {
+	return(n1-n2);
+}
 }
--- src/MyCalcPkg/MainCalc.java
+++ src/MyCalcPkg/MainCalc.java
@@ -6,7 +6,9 @@ public class MainCalc {
 		// TODO Auto-generated method stub
       Calculator c=new Calculator();
       int a=c.addition(10,20);
+      int b=c.Subtraction(40,20);
       System.out.println(a);
+      System.out.println(b);
 	}
 
 }
